a4bc41d0a55d92438d80c6c81674d5b2b45d37d8
Angela angie <angelajk496@gmail.com>
2026-05-08 16:21:25 +0300

    added java

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
  console.log("hello world");
+ // variable declaration
+ let first_Name = "michael"
+ console.log("michael")
+ let age=30
+ console.log(age)
+ let mpesa_balance=90.0
+ console.log(90.0)
+ let email_adress="@user.com"
+ console.log(email_adress)
+ let total = 5+3;
+ console.log(total)
+ age =18;
+ if(age < 18){
+    console.log("minor")
+ }
+ else(age > 18);{
+    console.log("adult")
+ }
+ // function declaration
+ function greet(){
+    console.log("hello")
+    alert("good bye class")
+ }
+ greet()
+function greet(michael){
+    console.log("lord"+name);
+}
+calling:
+greet("michael");
+ 
